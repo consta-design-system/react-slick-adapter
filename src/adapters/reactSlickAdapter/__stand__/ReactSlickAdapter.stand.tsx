14d747a3016624300115578460e13b587523a777
Maxim Kolesnikov <44449561+N1MBER@users.noreply.github.com>
2023-02-02 18:08:31 +0300

    docs(reactSlickAdapter): add links to figma and codesandbox (#3)
    closes consta-design-system/uikit#2856

--- src/adapters/reactSlickAdapter/__stand__/ReactSlickAdapter.stand.tsx
+++ src/adapters/reactSlickAdapter/__stand__/ReactSlickAdapter.stand.tsx
@@ -7,4 +7,7 @@ export default createStand({
   description: 'Адаптер для react-slick',
   version: '0.0.1',
   status: 'canary',
+  sandbox: 'reactslickadapter-4szur6',
+  figma:
+    'https://www.figma.com/embed?embed_host=share&url=https://www.figma.com/file/LKYyjgQ3khFHvXraZAG1n1/Consta-Components?node-id%3D2233%253A103376%26t%3DYAxn1ZwzfAl4Ga8r-1',
 });
--- src/mixs/ReactSlick/__stand__/ReactSlick.stand.tsx
+++ src/mixs/ReactSlick/__stand__/ReactSlick.stand.tsx
@@ -7,4 +7,6 @@ export default createStand({
   description: 'Миксин для стилизации слайдера.',
   version: '0.0.1',
   status: 'stable',
+  figma:
+    'https://www.figma.com/embed?embed_host=share&url=https://www.figma.com/file/LKYyjgQ3khFHvXraZAG1n1/Consta-Components?node-id%3D2233%253A103376%26t%3DYAxn1ZwzfAl4Ga8r-1',
 });
